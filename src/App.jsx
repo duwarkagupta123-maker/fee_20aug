f032950f5abd5b863b19a1bbe7ce224353813563
duwarkagupta123-maker <duwarkagupta123@gmail.com>
2026-08-21 00:32:34 +0530

    home page error correction

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       </nav>
 
       <Routes>
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/accordian" element={<Accordian />} />
       </Routes>
     </BrowserRouter>
